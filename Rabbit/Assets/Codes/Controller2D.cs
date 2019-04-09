0fec4b9586d8970bd0924d2df5d5e35d41d1fc97
Xinzzz <296232375@qq.com>
2019-04-09 20:00:03 +0800

    04/09

---
+++ Rabbit/Assets/Codes/Controller2D.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Controller2D : MonoBehaviour
+{
+    const float skinWidth = .015f;
+    BoxCollider2D collider;
+    RaycastOrigins raycastOrigins;
+    public CollisionInfo collsions;
+    public LayerMask collsionMask;
+
+    public int horizontalRayCount = 4;
+    public int verticalRayCount = 4;
+
+    float horizontalRaySpacing;
+    float verticalRaySpacing;
+
+    void Start()
+    {
+        collider = GetComponent<BoxCollider2D>();
+        CaculateRaySpacing();
+    }
+
+
+    struct RaycastOrigins
+    {
+        public Vector2 topLeft, topRight;
+        public Vector2 bottomLeft, bottomRight;
+    }
+
+    public struct CollisionInfo
+    {
+        public bool above, below;
+        public bool left, right;
+
+        public void ResetCollision()
+        {
+            above = below = false;
+            left = right = false;
+        }
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        UpdateRaycastOrigins();
+        collsions.ResetCollision();
+        if(velocity.x != 0)
+        {
+            HorizontalCollisions(ref velocity);
+        }
+        if(velocity.y != 0)
+        {
+            VerticalCollisions(ref velocity);
+        }
+               
+        transform.Translate(velocity);
+
+    }
+
+    void VerticalCollisions(ref Vector3 velocity)
+    {
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + skinWidth;
+
+        for (int i = 0; i < verticalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, collsionMask);
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY, Color.red);
+
+            if(hit)
+            {
+                velocity.y = (hit.distance - skinWidth) * directionY;
+                rayLength = hit.distance;
+
+                collsions.above = (directionY == 1);
+                collsions.below = (directionY == -1);
+            }
+        }
+    }
+
+    void HorizontalCollisions(ref Vector3 velocity)
+    {
+        float directionX = Mathf.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, collsionMask);
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX, Color.blue);
+            
+            if (hit)
+            {
+                velocity.x = (hit.distance - skinWidth) * directionX;
+                rayLength = hit.distance;
+
+                collsions.right = (directionX == 1);
+                collsions.left = (directionX == -1);
+            }
+        }
+    }
+
+    void UpdateRaycastOrigins()
+    {
+        Bounds bound = collider.bounds;
+        bound.Expand(skinWidth * -2);
+
+        raycastOrigins.topLeft = new Vector2(bound.min.x, bound.max.y);
+        raycastOrigins.topRight = new Vector2(bound.max.x, bound.max.y);
+        raycastOrigins.bottomLeft = new Vector2(bound.min.x, bound.min.y);
+        raycastOrigins.bottomRight = new Vector2(bound.max.x, bound.min.y);
+    }
+
+    void CaculateRaySpacing()
+    {
+        Bounds bound = collider.bounds;
+        bound.Expand(skinWidth * -2);
+
+        horizontalRayCount = Mathf.Clamp(horizontalRayCount, 2, int.MaxValue);
+        verticalRayCount = Mathf.Clamp(verticalRayCount, 2, int.MaxValue);
+
+        horizontalRaySpacing = bound.size.y / (horizontalRayCount - 1);
+        verticalRaySpacing = bound.size.x / (verticalRayCount - 1);
+    }
+}
---
+++ Rabbit/Assets/Codes/Player.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
+    Controller2D controller;
+
+    public float moveSpeed = 6f;
+
+    //jump variable
+    public float jumpHeight = 4f;
+    public float timeToJumpApex = .4f;
+    float gravity;
+    float jumpVelocity;
+
+    Vector3 velocity;
+
+    //flip character
+    bool facingRight = false;
+    SpriteRenderer sprite;
+
+    //animations
+    Animator anim;
+
+    void Start()
+    {
+        controller = GetComponent<Controller2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        gravity = (2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = gravity * timeToJumpApex;
+    }
+
+    private void FixedUpdate()
+    {
+        //Reset Y velocity
+        if(controller.collsions.above || controller.collsions.below)
+        {
+            velocity.y = 0;
+        }
+        //Input
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        //jump and gravity
+        if(input.y > 0 && controller.collsions.below)
+        {
+            velocity.y = jumpVelocity;
+        }
+        velocity.y -= gravity * Time.fixedDeltaTime;
+
+        velocity.x = input.x * moveSpeed;
+        anim.SetFloat("SpeedX", Mathf.Abs(input.x));
+        controller.Move(velocity);
+
+        //flip
+        if(input.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if(input.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 charScale = transform.localScale;
+        charScale.x *= -1;
+        transform.localScale = charScale;
+    }
+}
